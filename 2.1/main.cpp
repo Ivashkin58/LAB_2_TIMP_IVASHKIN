5e1959d6a42985d6e25b07f71c6f80642d3530d5
Ivashkin58 <115586011+Ivashkin58@users.noreply.github.com>
2023-12-05 22:17:11 +0300

    main.cpp

---
+++ 2.1/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cctype>
+#include "modAlphaCipher.h"
+#include <locale>
+#include <codecvt>
+#include <typeinfo>
+using namespace std;
+void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
+{
+    try {
+        wstring cipherText;
+        wstring decryptedText;
+        modAlphaCipher cipher(key);
+        cipherText = cipher.encrypt(Text);
+        if (destructCipherText)
+            cipherText.front() = towlower(cipherText.front());
+        decryptedText = cipher.decrypt(cipherText);
+        wcout<<L"key="<<key<<endl;
+        wcout<<Text<<endl;
+        wcout<<cipherText<<endl;
+        wcout<<decryptedText<<endl;
+    } catch (const cipher_error & e) {
+        wcerr<<"Error: "<<e.what()<<endl;
+    }
+    
+}
+
+int main()
+{
+    check(L"student",L"dmitriy");
+    check(L"dmitriy",L"");
+    check(L"dmitriy",L"student123");
+    check(L"d m i t r i y",L"STUDENT");
+    check(L"123",L"STUDENT");
+}
